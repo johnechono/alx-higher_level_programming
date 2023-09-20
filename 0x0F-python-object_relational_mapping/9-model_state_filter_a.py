609d9bd946456a34474bbaeeed454640d2481b6f
johnechono <johninalegwu22@gmail.com>
2023-09-20 11:49:59 -0700

    corrections effected

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""A script that lists all State
-objects that contains the letter a
- from the database hbtn_0e_6_usa"""
+""" lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa """
 
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state in session.query(State)\
-                        .filter(State.name.like('%a%'))\
-                        .order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}") if 'a' in state.name else ""
